1eb1a358f7ad72bf8d39cc56a322da26fdbe5732
James Campbell <james@supmenow.com>
2017-05-09 16:03:19 +0100

    All documented.

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ var CURRENT_TOPIC = null
 var MAIN_TOPIC_ID = "main"
 var START_TOPIC_EVENT = "start_topic"
 
-function createTopicHear(bp) {
-    return function (condition, callback) {
-        condition.topic = identifier
-        bp.hear(condition, callback)
-    }
-}
-
-function wrapBotHear(hearMethod) {
+// This method wraps the original bot hear method
+// with one that makes sure all listeners default
+// to the "Main" topic. This will probably go
+// away once integrated into the core.
+//
+function _wrapBotHear(hearMethod) {
     return function(condition, callback) {
 
         if (!condition.topic) {
@@ -27,10 +25,26 @@ function wrapBotHear(hearMethod) {
     }
 }
 
+// This method creates a topic. It takes both an
+// identifier and constructor.
+//
+// The constructor will be called and passed in a
+// topic object.
+//
+// This function will return the identifier for 
+// bot to use to start this topic with.
+//
 function createTopic(identifier, constructor) {
+
+    // Create an object for the topic
+    // with an identifier and a hear method
+    // with creates listeners for the current topic
     var topic = {
         id: identifier,
-        hear: createTopicHear(bp)
+        hear: function (condition, callback) {
+            condition.topic = identifier
+            bp.hear(condition, callback)
+        }
     }
 
     constructor(topic)
@@ -38,6 +52,12 @@ function createTopic(identifier, constructor) {
     return identifier
 }
 
+// This method start's a topic.
+// 
+// This method takes
+// the identifier of the topic to start and
+// the event it was started from.
+//
 function startTopic(identifier, event) {
 
     if (TOPIC_STACK.length > DEFAULT_MAX_TOPIC_STACK) {
@@ -49,19 +69,29 @@ function startTopic(identifier, event) {
     }
     
     CURRENT_TOPIC = identifier
-    emitStartTopic(bp, event)
+    _emitStartTopic(bp, event)
 }
 
+// This method returns to the main topic
+// and clear's the topic stack.
+// 
+// This method takes the event it was started from.
+//
 function returnToMainTopic(event) {
     TOPIC_STACK = []
     CURRENT_TOPIC = MAIN_TOPIC_ID
-    emitStartTopic(bp, event)
+    _emitStartTopic(bp, event)
 }
 
+// This method goes to the last started topic
+// in the topic stack.
+// 
+// This method takes the event it was started from.
+//
 function endTopic(event) {
     if (TOPIC_STACK.length > 0) {
         CURRENT_TOPIC = TOPIC_STACK.pop()
-        emitStartTopic(bp, event)
+        _emitStartTopic(bp, event)
     } else {
         // This shouldn't happen
         // but incase it does take the bot back
@@ -70,7 +100,17 @@ function endTopic(event) {
     }
 }
 
-function emitStartTopic(bp, event) {
+// This emits the start_topic event
+// when a topic is started or returned
+// to.
+//
+// We try to preserve any properties the calling
+// logic added to the event the topic was started from.
+//
+// But overwrite all the properties needed to let the
+// code know this is a start_topic event
+//
+function _emitStartTopic(bp, event) {
     bp.middlewares.sendIncoming(Object.assign({}, event, {
         topic: CURRENT_TOPIC,
         platform: event.platform,
@@ -81,7 +121,15 @@ function emitStartTopic(bp, event) {
     }))
 }
 
-function incomingMiddleware(event, next) {
+// The main middlewear code.
+//
+// If we don't have a topic make sure we return
+// to the 'main' topic.
+//
+// Then inject the current topic into the 
+// event object
+//
+function _incomingMiddleware(event, next) {
 
     if (!CURRENT_TOPIC) {
         bp.returnToMainTopic(event)
@@ -97,7 +145,7 @@ function startTopics(bp) {
         name: 'topics',
         type: 'incoming',
         order: 100,
-        handler: incomingMiddleware,
+        handler: _incomingMiddleware,
         module: package.name,
         description: 'Middleware for Topics'
     })
@@ -110,7 +158,7 @@ function startTopics(bp) {
 
     // We wrap the original hear method so that
     // it defaults to listening to the "main" topic
-    bp.hear = wrapBotHear(bp.hear)    
+    bp.hear = _wrapBotHear(bp.hear)    
 }
 
 module.exports = {
